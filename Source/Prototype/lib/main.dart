ea63d3dc8ebf767bcf30827e237b21c8d1e576de
anamargaridarl <anamargaridarl@gmail.com>
2019-10-22 19:55:57 +0100

    started to implement search page

--- Source/Prototype/lib/main.dart
+++ Source/Prototype/lib/main.dart
@@ -13,14 +13,15 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: '/',
       routes: {
-        '/': (context) => MyHomePage(),
+        '/': (context) => Search(),
         '/Options': (context) => OptionsMenu(),
-        '/Lectures' : (context) => Lectures(),
-        '/Networking' : (context) => Networking(),
-        '/Food' : (context) => Food(),
+        '/Lectures': (context) => Lectures(),
+        '/Networking': (context) => Networking(),
+        '/Food': (context) => Food(),
         '/Workshops': (context) => Workshops(),
         '/Wc': (context) => Wc(),
         '/Exits': (context) => Exits(),
+        //'/Search': (context) => Search(),
       },
     );
   }
@@ -67,23 +68,24 @@ class App_name extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child : Text("SimplyFind", style: TextStyle(color: Colors.blue)),
+      child: Text("SimplyFind", style: TextStyle(color: Colors.blue)),
     );
   }
 }
-
 
 class OptionsMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("Options"),),
-      body: SafeArea(
-        child: Container(
-          color: Color(0xFFACDF87) ,
+        appBar: AppBar(
+          title: Text("Options"),
+        ),
+        body: SafeArea(
+            child: Container(
+          color: Color(0xFFACDF87),
           child: Stack(
             children: <Widget>[
-         /*     ButtonWithImage(image: "images/speech.png", onPressed: (){
+              /*     ButtonWithImage(image: "images/speech.png", onPressed: (){
                 Navigator.pushNamed(context, '/Lectures');
               }, ),
               ButtonWithImage(image: "images/network.png", onPressed: (){
@@ -95,30 +97,53 @@ class OptionsMenu extends StatelessWidget {
               ButtonWithImage(image: "images/restaurant.png", onPressed: (){
                 Navigator.pushNamed(context, '/Food');
               }, ),*/
-             // IconsPics(x: 10, y: 10, image: "images/speech.png"),
-              MyButton(x: 10, y:10, title: "Lectures", onPressed: (){
-                Navigator.pushNamed(context, '/Lectures');
-              }, ),
-              MyButton(x: 60, y:10, title: "Networking", onPressed: (){
-                Navigator.pushNamed(context, '/Networking');
-              }),
-              MyButton(x: 10, y:35, title: "Food", onPressed: (){
-                Navigator.pushNamed(context, '/Food');
-              }),
-              MyButton(x: 60, y:35, title: "Workshops", onPressed: (){
-                Navigator.pushNamed(context, '/Workshops');
-              }),
-              MyButton(x: 10, y:60, title: "wc", onPressed: (){
-                Navigator.pushNamed(context, '/Wc');
-              }),
-              MyButton(x: 60, y:60, title: "exits", onPressed: (){
-                Navigator.pushNamed(context, '/Exits');
-              })
+              // IconsPics(x: 10, y: 10, image: "images/speech.png"),
+              MyButton(
+                x: 10,
+                y: 10,
+                title: "Lectures",
+                onPressed: () {
+                  Navigator.pushNamed(context, '/Lectures');
+                },
+              ),
+              MyButton(
+                  x: 60,
+                  y: 10,
+                  title: "Networking",
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/Networking');
+                  }),
+              MyButton(
+                  x: 10,
+                  y: 35,
+                  title: "Food",
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/Food');
+                  }),
+              MyButton(
+                  x: 60,
+                  y: 35,
+                  title: "Workshops",
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/Workshops');
+                  }),
+              MyButton(
+                  x: 10,
+                  y: 60,
+                  title: "wc",
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/Wc');
+                  }),
+              MyButton(
+                  x: 60,
+                  y: 60,
+                  title: "exits",
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/Exits');
+                  })
             ],
           ),
-        )
-      )
-    );
+        )));
   }
 }
 
@@ -154,8 +179,8 @@ class IconsPics extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Positioned(
-      left: (x/100)*MediaQuery.of(context).size.width,
-      top: (y/100)*MediaQuery.of(context).size.height,
+      left: (x / 100) * MediaQuery.of(context).size.width,
+      top: (y / 100) * MediaQuery.of(context).size.height,
       child: Container(
           width: 110.0,
           height: 100.0,
@@ -175,36 +200,28 @@ class BigButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Positioned(
-      left: (x / 100) * MediaQuery
-          .of(context)
-          .size
-          .width,
-      top: (y / 100) * MediaQuery
-          .of(context)
-          .size
-          .height,
+      left: (x / 100) * MediaQuery.of(context).size.width,
+      top: (y / 100) * MediaQuery.of(context).size.height,
       child: Container(
           width: 270.0,
           height: 50.0,
           margin: EdgeInsets.symmetric(vertical: 3.0),
           child: SizedBox.expand(
               child: OutlineButton(
-                child: Text(title),
-                borderSide: BorderSide(
-                  color: Color(0xFFA5D6A7),
-                  style: BorderStyle.solid, width: 2.8,
-                ),
-                onPressed: onPressed,
-              )
-          )
-      ),
+            child: Text(title),
+            borderSide: BorderSide(
+              color: Color(0xFFA5D6A7),
+              style: BorderStyle.solid,
+              width: 2.8,
+            ),
+            onPressed: onPressed,
+          ))),
     );
   }
 }
 
 class MyButton extends StatelessWidget {
-
-  MyButton({this.x, this.y, this.title, this.onPressed });
+  MyButton({this.x, this.y, this.title, this.onPressed});
   final int x;
   final int y;
   final String title;
@@ -213,22 +230,26 @@ class MyButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Positioned(
-      left: (x/100)*MediaQuery.of(context).size.width,
-      top: (y/100)*MediaQuery.of(context).size.height,
-      child: Container(
-          width: 110.0,
-          height: 100.0,
-          margin: EdgeInsets.symmetric(vertical: 3.0),
-         child: OutlineButton(
-              child: Text(title, style: TextStyle(color: Color(0xFF083663),),),
-                borderSide: BorderSide(
-                color: Color(0xFFA5D6A7),
-                style: BorderStyle.solid, width: 2.8,
+        left: (x / 100) * MediaQuery.of(context).size.width,
+        top: (y / 100) * MediaQuery.of(context).size.height,
+        child: Container(
+            width: 110.0,
+            height: 100.0,
+            margin: EdgeInsets.symmetric(vertical: 3.0),
+            child: OutlineButton(
+              child: Text(
+                title,
+                style: TextStyle(
+                  color: Color(0xFF083663),
                 ),
-                onPressed: onPressed,
-            )
-          )
-      );
+              ),
+              borderSide: BorderSide(
+                color: Color(0xFFA5D6A7),
+                style: BorderStyle.solid,
+                width: 2.8,
+              ),
+              onPressed: onPressed,
+            )));
   }
 }
 
@@ -237,24 +258,29 @@ class Lectures extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Lectures"),),
+        title: Text("Lectures"),
+      ),
       body: Center(
         child: Image.asset('images/wc.png'),
       ),
     );
   }
-
 }
 
 class Networking extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Networking"), ),
-      body: Center(
-        child: Text("MyNetworking", style: TextStyle(color: Color(0xFF2222FF),)
-          ,)
+      appBar: AppBar(
+        title: Text("Networking"),
       ),
+      body: Center(
+          child: Text(
+        "MyNetworking",
+        style: TextStyle(
+          color: Color(0xFF2222FF),
+        ),
+      )),
     );
   }
 }
@@ -263,7 +289,9 @@ class Food extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Food'),),
+      appBar: AppBar(
+        title: Text('Food'),
+      ),
       body: Center(
         child: Text("MyFood"),
       ),
@@ -271,25 +299,27 @@ class Food extends StatelessWidget {
   }
 }
 
-
 class Workshops extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Workshops"),),
+      appBar: AppBar(
+        title: Text("Workshops"),
+      ),
       body: Center(
         child: Text("MyWorkshops"),
       ),
     );
   }
-
 }
 
 class Wc extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("wc"),),
+      appBar: AppBar(
+        title: Text("wc"),
+      ),
       body: Center(
         child: Text("wc"),
       ),
@@ -301,9 +331,147 @@ class Exits extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(' Exits'),),
+      appBar: AppBar(
+        title: Text(' Exits'),
+      ),
       body: Center(
         child: Text("My Exits"),
+      ),
+    );
+  }
+}
+
+Color hexToColor(String code) {
+  return new Color(int.parse(code.substring(1, 7), radix: 16) + 0xFF000000);
+}
+
+class MyCustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final double height;
+
+  const MyCustomAppBar({
+    Key key,
+    @required this.height,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 190,
+          color: Color.fromRGBO(7, 55, 99,1),
+          child: new Form(
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: 40),
+                Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.arrow_back,
+                      color: Colors.white,
+                      size: 40.0,
+                      semanticLabel: 'Go back',
+                    ),
+                    Expanded(
+                      child: new TextFormField(
+                        decoration: new InputDecoration(
+                          filled: true,
+                          labelText: 'My Localization',
+                          fillColor: Colors.white,
+                          contentPadding: new EdgeInsets.symmetric(
+                              vertical: 18.0, horizontal: 10.0),
+                          border: new OutlineInputBorder(
+                            borderRadius: new BorderRadius.circular(30.0),
+                            borderSide: new BorderSide(),
+                          ),
+                          prefixIcon: Padding(
+                            padding: EdgeInsets.all(0.0),
+                            child: Icon(
+                              Icons.search,
+                              color: Colors.black,
+                            ), // icon is 48px widget.
+                          ),
+                        ), // icon is 48px widge
+                        validator: (val) {
+                          if (val.length == 0) {
+                            return "Email cannot be empty";
+                          } else {
+                            return null;
+                          }
+                        },
+                        keyboardType: TextInputType.emailAddress,
+                        style: new TextStyle(
+                          fontFamily: "Poppins",
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Row(
+                  children: <Widget>[
+                    SizedBox(width: 40),
+                    Expanded(
+                      child: new TextFormField(
+                        decoration: new InputDecoration(
+                          labelText: 'Choose Destination',
+                           filled: true,
+                          fillColor: Colors.white,
+                          contentPadding: new EdgeInsets.symmetric(
+                              vertical: 18.0, horizontal: 5.0),
+                          border: new OutlineInputBorder(
+                            borderRadius: new BorderRadius.circular(30.0),
+                            borderSide: new BorderSide(),
+                          ),
+                          prefixIcon: Padding(
+                            padding: EdgeInsets.all(0.0),
+                            child: Icon(
+                              Icons.search,
+                              color: Colors.grey,
+                            ), // icon is 48px widget.
+                          ),
+                        ), // icon is 48px widge
+                        validator: (val) {
+                          if (val.length == 0) {
+                            return "Email cannot be empty";
+                          } else {
+                            return null;
+                          }
+                        },
+                        keyboardType: TextInputType.emailAddress,
+                        style: new TextStyle(
+                          fontFamily: "Poppins",
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+        new Container(
+  height: 30,
+  color: Color.fromRGBO(182, 215, 168, 1),
+)
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(height);
+}
+
+class Search extends StatelessWidget {
+  @override
+  Widget build(BuildContext ctxt) {
+    return new MaterialApp(
+      title: "SearchPage",
+      home: new Scaffold(
+        appBar: new MyCustomAppBar(height: 250),
+        body: new Center(
+          child: new Text("Hello "),
+        ),
       ),
     );
   }
