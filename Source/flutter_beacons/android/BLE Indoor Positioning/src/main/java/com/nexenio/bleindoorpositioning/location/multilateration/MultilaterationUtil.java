bf58b71b43b2a1cb767f994d2302af1ee45be5b1
João Martins <up201707311@fe.up.pt>
2019-11-05 13:11:37 +0000

    changing technology, removing project and closing branch

--- Source/flutter_beacons/android/BLE Indoor Positioning/src/main/java/com/nexenio/bleindoorpositioning/location/multilateration/MultilaterationUtil.java
+++
@@ -1,9 +0,0 @@
-package com.nexenio.bleindoorpositioning.location.multilateration;
-
-/**
- * Created by steppschuh on 14.12.17.
- */
-
-public abstract class MultilaterationUtil {
-
-}
--- Source/flutter_beacons/lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'BluetoothWidgets.dart';
-
-void main() {
-  runApp(FlutterBlueApp());
-}
-
