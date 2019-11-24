1218496f3e2d12bc71f9c3f6dc6dbd9bd3e8c0fe
Marantesss <up201706917@fe.up.pt>
2019-11-24 15:50:18 +0000

    Event documentation

--- SimplyFind/lib/POI/Event.dart
+++ SimplyFind/lib/POI/Event.dart
@@ -3,23 +3,39 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'PointOfInterest.dart';
 
 class Event extends PointOfInterest {
+  /// Name of the event
   final String _name;
+  /// Name of the speaker responsible
   final String _speaker;
+  /// Name of the room in which the event will take place
   final String _room;
+  /// Small description about the content of the event
   final String _description;
+  /// List containing the tags related to the event
   final List<String> _tags;
   //final String _date;
 
+  /// Constructor
   Event(this._name, this._speaker, this._room, this._description, this._tags, int pointId, LatLng location, int floor) : super(pointId, location, floor);
 
+  /// Getter member function for [_name]
+  /// return a [String] with the name of the event
   String get name => _name;
 
+  /// Getter member function for [_speaker]
+  /// return a [String] with the name of the speaker for the event
   String get speaker => _speaker;
 
+  /// Getter member function for [_room]
+  /// return a [String] with the name of the room for the event
   String get room => _room;
 
+  /// Getter member function for [_description]
+  /// return a [String] with the description for the event
   String get description => _description;
 
+  /// Getter member function for [_tags]
+  /// return a [String] with the name of the event
   List<String> get tags => _tags;
 
   //String get date => _date;
