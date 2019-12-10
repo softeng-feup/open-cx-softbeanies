270568f920a670ccc8e0d3aca0805b3ac60fff83
João Miguel Martins <37221671+joaomrcsmartins@users.noreply.github.com>
2019-12-10 14:39:42 +0000

    Update main.dart

--- SimplyFind/lib/main.dart
+++ SimplyFind/lib/main.dart
@@ -4,7 +4,6 @@ import 'src/App.dart';
 
 
 Future main() async {
-  WidgetsFlutterBinding.ensureInitialized();
   DataServer ds = DataServer();
   // wait for server to load information
   await ds.loadData();
